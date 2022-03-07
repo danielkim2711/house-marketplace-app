d845d09e4aa466f17a0be56020bf4956c2ebc875
Daniel <danielkim2711@gmail.com>
2022-03-07 19:25:48 +1300

    Define Explore page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
+import Category from './pages/Category';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
@@ -27,7 +29,7 @@ function App() {
         <Navbar />
       </Router>
 
-      <ToastContainer />
+      <ToastContainer autoClose={3000} />
     </>
   );
 }
