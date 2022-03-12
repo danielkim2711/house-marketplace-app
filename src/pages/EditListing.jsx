744b32834f3e7fe8345ab342eee3711988d02101
Daniel <danielkim2711@gmail.com>
2022-03-12 21:07:14 +1300

    Clear up console warnings

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -137,15 +137,10 @@ function EditListing() {
         uploadTask.on(
           'state_changed',
           (snapshot) => {
-            const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
             switch (snapshot.state) {
               case 'paused':
-                console.log('Upload is paused');
                 break;
               case 'running':
-                console.log('Upload is running');
                 break;
               default:
                 break;
